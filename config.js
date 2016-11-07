72fc73be3bdc1af5f3c037ec6a0ccafd0e7b7fa5
Thomas WOLFF <thomas.wolff@cpcoop.fr>
2016-11-07 20:41:27 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,12 +5,12 @@ var GH = {}
 GH.SECRET = 'YWU0ZmRkZGFjOTVlZGM1ZTc1MmI3NjRjZTI3Y2UxZGYyMzdmZTdkZg=='
 GH.ID = 'ODUzMjY1YjA5YjBjMjVlOTg2MTQ='
 // Github Owner's
-GH.OWNER = 'multibao'
+GH.OWNER = 'labua'
 // Github crew's repository
-GH.CREW = 'organisations'
+GH.CREW = 'labua/organisations'
 
 var MULTIBAO = {}
-MULTIBAO.UVP1 = 'Partager et enrichir vos pratiques d\'équipe'
-MULTIBAO.UVP2 = 'Let\'s Do It Together'
-MULTIBAO.BUTTON1 = 'Découvrir'
-MULTIBAO.BUTTON2 = 'Espace contributeurs / bloggeurs'
+MULTIBAO.UVP1 = 'Pratiques pédagogiques innovantes'
+MULTIBAO.UVP2 = '8 novembre 2016'
+MULTIBAO.BUTTON1 = 'C\'est quoi cette techno?'
+MULTIBAO.BUTTON2 = 'Contribuer à entretenir les fiches'
